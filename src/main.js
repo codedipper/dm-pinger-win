5b9ea708b01375e8bd0f2f8a1051d2de546ea5c5
codedipper <59625582+codedipper@users.noreply.github.com>
2020-08-14 13:22:00 -0400

    Added project's files.

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const { botToken, userID, pingInterval } = require("./config.json");
+const DSC = require("discord.js");
+const Client = new DSC.Client(botToken);
+
+Client.on("ready", () => {
+    console.log("Bot is up and up running.");
+
+    function sendPing(){
+        let pingUser = Client.users.cache.get(userID);
+
+        pingUser.send("Here is your ping.");
+    }
+
+    setInterval(sendPing, parseInt(pingInterval) * 1000);
+});
+
+Client.on("error", () => {
+    console.error();
+    Client.login(botToken);
+});
+
+Client.login(botToken);
