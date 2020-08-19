5003067944d9f75e5e8a824c01bf34ca00645fcc
codedipper <59625582+codedipper@users.noreply.github.com>
2020-08-19 19:01:00 -0400

    Added the rest of the files.
    Nothing changed except a line break.

---
+++ src/start.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo Installing the dependencies..
+call npm ci
+
+if NOT ["%errorlevel%"]==["0"] (
+  pause
+  exit /b %errorlevel%
+)
+
+echo Starting up the bot..
+call npm run start
+
+if NOT ["%errorlevel%"]==["0"] (
+  pause
+  exit /b %errorlevel%
+)
